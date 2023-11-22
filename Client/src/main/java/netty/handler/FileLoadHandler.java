97145b180dfaa98630699a67e3986e02a79cd2be
yuna <cyunanne@gmail.com>
2023-11-22 14:37:18 +0900

    if-else if 문 구성으로 변경

--- Client/src/main/java/netty/handler/FileLoadHandler.java
+++ Client/src/main/java/netty/handler/FileLoadHandler.java
@@ -4,6 +4,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPromise;
 import io.netty.handler.stream.ChunkedFile;
+import netty.common.FileSpec;
 import netty.common.Header;
 
 import java.io.FileNotFoundException;
@@ -20,28 +21,34 @@ public class FileLoadHandler extends ChannelOutboundHandlerAdapter {
 
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
-        if( !(msg instanceof String) ) {
+        
+        // 메타 데이터
+        if(msg instanceof FileSpec) {
             ctx.writeAndFlush(msg);
-            return;
         }
 
-        try {
+        // 파일 데이터
+        else if(msg instanceof String) {
             String filePath = (String) msg;
-            RandomAccessFile file = new RandomAccessFile(filePath, "r");
-            ChunkedFile chunkedFile = new ChunkedFile(file, 0, file.length(), Header.CHUNK_SIZE);
-            ctx.writeAndFlush(chunkedFile);
 
-            // Stream
+            try {
+                RandomAccessFile file = new RandomAccessFile(filePath, "r");
+                ChunkedFile chunkedFile = new ChunkedFile(file, 0, file.length(), Header.CHUNK_SIZE);
+                ctx.writeAndFlush(chunkedFile);
+
+                // Stream
 //            FileInputStream fis = new FileInputStream(filePath);
 //            ChunkedStream chunkedStream = new ChunkedStream(fis);
 //            ctx.writeAndFlush(chunkedStream)
 
-        } catch (FileNotFoundException e) {
-            System.out.println("파일을 찾을 수 없습니다.");
-            ctx.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            } catch (FileNotFoundException e) {
+                System.out.println("파일을 찾을 수 없습니다.");
+                ctx.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
+
     }
 
     @Override
