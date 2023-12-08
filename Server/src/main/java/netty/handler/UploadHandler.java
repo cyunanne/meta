faf0e9eb15a0487340a9d9c2b38171fdbf3623b2
yuna <cyunanne@gmail.com>
2023-12-08 13:26:06 +0900

    ObjectOutputStream 닫기

--- Server/src/main/java/netty/handler/UploadHandler.java
+++ Server/src/main/java/netty/handler/UploadHandler.java
@@ -15,6 +15,7 @@ import java.io.ObjectOutputStream;
 public class UploadHandler extends ChannelInboundHandlerAdapter {
 
     private FileOutputStream fos;
+    private ObjectOutputStream oos;
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
@@ -29,13 +30,15 @@ public class UploadHandler extends ChannelInboundHandlerAdapter {
             String filePath = filespec.getFilePath();
             FileUtils.mkdir(filePath);
             fos = new FileOutputStream(filePath);
-            new ObjectOutputStream(fos).writeObject(filespec); // 메타 데이터 저장
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(filespec); // 메타 데이터 저장
         }
 
         // 파일 수신
         if(fos != null) {
             fos.getChannel().write(byteBuf.nioBuffer());
             if(header.isEof()) {
+                oos.close();
                 fos.close();
                 ctx.close();
             }
